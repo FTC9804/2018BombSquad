0e695baaf18d7b6a7b3fa4744149a7d90477a00d
Steve Cox <stephencox@verizon.net>
2017-09-30 20:27:43 -0700

    javadoc commenting with all of our controls for drivers and gunenrs
    Designed by Steve Cox after input from Kevin Mardirossian 9-30-17

---
+++ PseudoCodeControls.java
@@ -0,0 +1,51 @@
+/**
+ * Designed by Steve Cox after input from Kevin Mardirossian 9-30-17
+ * Version 1.0 with initial button mapping
+ *
+ *
+ *       _                 _   _      _
+ *      (_)               | | (_)    | |
+ *       _  ___  _   _ ___| |_ _  ___| | __
+ *      | |/ _ \| | | / __| __| |/ __| |/ /
+ *      | | (_) | |_| \__ \ |_| | (__|   <
+ *      | |\___/ \__, |___/\__|_|\___|_|\_\
+ *     _/ |       __/ |
+ *    |__/       |___/
+ *
+ *
+ *
+ * DRIVER Controls
+ *      Joystick
+ *          Left X          Rotation
+ *          Right X         Strafe
+ *          Right Y         Forward/Backward Movement
+ *
+ * GUNNER Controls
+ *      Joystick
+ *          Left Y          Lifter Up/Down
+ *          Right X         Adjust Grabber in X direction
+ *
+ *      Triggers
+ *          Left            Variable Power Bottom Intake
+ *          Right           Variable Power Top Intake
+ *
+ *      Bumpers
+ *          Left            Bottom Spit Full Power
+ *          Right           Top Spit Full Power
+ *
+ *      Buttons
+ *          D-Pad Up        Release Bottom Grabber
+ *          D-Pad Down      Grab Bottom
+ *          Y               Release Top Grabber
+ *          A               Grab Top
+ *          START + BACK    Transition to End Game Button Mapping
+ *
+ * END GAME Controls
+ *  ------To Be Determined------
+ *
+ *
+ *
+ */
+
+
+
