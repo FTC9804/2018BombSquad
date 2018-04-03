9b2059e7c2b2f4ee1005c22e6ad224c4729f1958
mleher1 <mleher1@hwemail.com>
2018-04-02 22:52:53 -0700

    Update BLUEAUTONOTCORNER.java

--- LAMeet3/BLUEAUTONOTCORNER.java
+++ LAMeet3/BLUEAUTONOTCORNER.java
@@ -14,11 +14,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 //Autonomous to score ball and a block from the blue non-corner position
 public class BLUEAUTONOTCORNER extends FunctionsForAuto {
-    String vuMarkOutput = ""; //Set vuMarkOutput to the empty string
 
     public void runOpMode() throws InterruptedException { //runOpMode() method
 
-        configure("blue", "relicSide"); //Configure with parameters blue and relicSide
+        configure("blue", "notcorner"); //Configure with parameters blue and relicSide
 
         pause(.05); //pause for .05 seconds
 
@@ -28,15 +27,15 @@ public class BLUEAUTONOTCORNER extends FunctionsForAuto {
         telemetry.addData("calibrated", true);
         telemetry.update();
 
-        pause(.05); //pause for .5 seconds
+        pause(.05); //pause for .05 seconds
 
         introduceAngle(); //Introduce the angle
 
-        pause(.05); //pause for .5 seconds
+        pause(.05); //pause for .05 seconds
 
         waitForStart(); //Wait for start
 
-        pause(.05); //pause for .5 seconds
+        pause(.05); //pause for .05 seconds
 
         //Telemetry
         telemetry.addData("here", true);
@@ -46,37 +45,90 @@ public class BLUEAUTONOTCORNER extends FunctionsForAuto {
 
         pause(.15); //pause for .15 seconds
 
-        vuMarkOutput = detectVuMark(2); //Run vuforia detection for 3 seconds
-
-        pause(.15); //pause for .15 seconds
-
-        driveNewIMU(4.75, 10, -.25, false, 0); //Drive backwards 7.75 inches at -.25 power keeping a 0 degree heading with a 10 second limit
+        driveNewIMU(4.75, 10, -.25, false, 0); //Drive backwards 4.75 inches at -.25 power keeping a 0 degree heading with a 10 second limit
 
         pause(.8); //pause for .8 seconds
 
-        driveNewIMU(13, 10, -.25, false, 0); //Drive backwards 10.25 inches at -.25 power keeping a 0 degree heading with a 10 second limit
+        driveNewIMU(9.2, 10, -.25, false, 0);
 
-        pause(.4); //pause for .4 seconds
+        touchServo.setPosition(.45);
 
-        spinMove(178, false, 10); //Spin to 178 degrees not starting at .3 power with a 10 second timeout
+        pause(.75); //pause for .4 seconds
 
-        pause(.2); //pause for .45 seconds
-
-        if (vuMarkOutput.equalsIgnoreCase("left")) //If vuforia reads left
+        if (vuMarkReturn.equalsIgnoreCase("left")) //If vuforia reads left
         {
-            strafeNewIMU(4, 5, .65, 178); //Strafe to the right for 10.5 inches at .63 power keeping a 178 degree heading with a 5 second limit
+            strafeNewIMU(12, 5, .65, 0); //Strafe to the right for 4 inches at .65 power keeping a 178 degree heading with a 5 second limit
+
+            pause(.1);
+
+            spinMove(-52, false, 5, true);
+
+            pause(.5);
+
+            frontPanGrip.setPosition(.258);
+            backPanGrip.setPosition(.112);
+
+            pause(1);
+
+            leftPanSpin.setPosition(.525); //Set leftPanSpin to position .21, as this is the intaking glyph position
+            rightPanSpin.setPosition(.586);
+
+            pause(1);
+
+            driveNewIMU(16.5, 3.2, -.3, false, -52);
+
         }
-        else if (vuMarkOutput.equalsIgnoreCase("right")) //If vuforia reads right
+        else if (vuMarkReturn.equalsIgnoreCase("right")) //If vuforia reads right
         {
-            strafeNewIMU(17.9, 5, .65, 178); //Strafe to the right for 26.5 inches at .63 power keeping a 178 degree heading with a 5 second limit
+            spinMove(-60, false, 5, true);
+
+            pause(.5);
+
+            frontPanGrip.setPosition(.258);
+            backPanGrip.setPosition(.112);
+
+            pause(1);
+
+            leftPanSpin.setPosition(.525); //Set leftPanSpin to position .21, as this is the intaking glyph position
+            rightPanSpin.setPosition(.586);
+
+            pause(1);
+
+            driveNewIMU(16.5, 3.2, -.3, false, -60);
         }
         else //Else
         {
-            strafeNewIMU(12.5, 5, .65, 178); //Strafe to the right for 18.5 inches at .63 power keeping a 178 degree heading with a 5 second limit
+            spinMove(-45, false, 5, true);
+
+            pause(.5);
+
+            frontPanGrip.setPosition(.258);
+            backPanGrip.setPosition(.112);
+
+            pause(1);
+
+            leftPanSpin.setPosition(.525); //Set leftPanSpin to position .21, as this is the intaking glyph position
+            rightPanSpin.setPosition(.586);
+
+            pause(1);
+
+            driveNewIMU(16.5, 3.2, -.3, false, -45);
         }
 
         pause(.1); //pause for .1 seconds
 
-        touch(true, false, false); //Run touch method with parameters true, false, and false, meaning we are running from blue not corner position
+        if (vuMarkReturn.equalsIgnoreCase("left")) //If vuforia reads left
+        {
+            driveNewIMU(3, 5, .3, true, -52);
+        }
+        else if (vuMarkReturn.equalsIgnoreCase("right")) //If vuforia reads right
+        {
+            driveNewIMU(3, 5, .3, true, -60);
+        }
+        else //else
+        {
+            driveNewIMU(3, 5, .3, true, -45);
+        }
+
     }
 }
