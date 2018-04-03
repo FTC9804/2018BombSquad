317cacf387b78e85616d6fe62a800c8205e52c96
mleher1 <mleher1@hwemail.com>
2018-04-02 22:56:00 -0700

    Update REDAUTONOTCORNER.java

--- LAMeet3/REDAUTONOTCORNER.java
+++ LAMeet3/REDAUTONOTCORNER.java
@@ -45,63 +45,118 @@ public class REDAUTONOTCORNER extends FunctionsForAuto {
 
         dropFeelerMoveBallOnlyNewRobot(); //Run dropFeelerMoveBallOnlyNewRobot to score the ball
 
-        pause(.15); //pause for .15 seconds
+        pause(.05); //pause for .15 seconds
 
         driveNewIMU(6, 10, .25, true, 0); //Drive forward 6 inches at .25 power keeping a 0 degree heading with a 10 second limit
+
+        touchServo.setPosition(.45);
 
         pause(.75); //pause for .5 seconds
 
         if (vuMarkReturn.equalsIgnoreCase("left")) //If vuforia reads left
         {
-            strafeNewIMU(26, 5, -.65, 15); //Strafe to the left for 26 inches at -.65 power keeping a 15 degree heading with a 5 second limit
-        } else if (vuMarkReturn.equalsIgnoreCase("right")) //If vuforia reads right
+            spinMove(230, false, 5, true);
+
+            pause(.1);
+
+            frontPanGrip.setPosition(.258);
+            backPanGrip.setPosition(.112);
+
+            pause(1);
+
+            leftPanSpin.setPosition(.525); //Set leftPanSpin to position .21, as this is the intaking glyph position
+            rightPanSpin.setPosition(.586);
+
+            pause(1);
+
+            driveNewIMU(16.5, 3.2, -.3, false, 230);
+
+        }
+        else if (vuMarkReturn.equalsIgnoreCase("right")) //If vuforia reads right
         {
-            strafeNewIMU(10.4, 5, -.65, 15); //Strafe to the left for 10.4 inches at -.65 power keeping a 15 degree heading with a 5 second limit
-        } else //else
+            pivot(-150, 4);
+
+            leftPanSpin.setPosition(.93); //Set leftPanSpin to position .21 to lower thepan
+            rightPanSpin.setPosition(.9366);
+
+            pause(.8);
+
+            frontPanGrip.setPosition(.258);
+            backPanGrip.setPosition(.112);
+
+            pause(1);
+
+            leftPanSpin.setPosition(.525); //Set leftPanSpin to position .21, as this is the intaking glyph position
+            rightPanSpin.setPosition(.586);
+
+            pause(1);
+
+            driveNewIMU(12.5, 2.5, -.3, false, -150);
+        }
+        else //else
         {
-            strafeNewIMU(17.4, 5, -.65, 15); //Strafe to the left for 17.4 inches at -.65 power keeping a 15 degree heading with a 5 second limit
+            spinMove(200, false, 5, true);
+
+            pause(.1);
+
+            frontPanGrip.setPosition(.258);
+            backPanGrip.setPosition(.112);
+
+            pause(1);
+
+            leftPanSpin.setPosition(.525); //Set leftPanSpin to position .21, as this is the intaking glyph position
+            rightPanSpin.setPosition(.586);
+
+            pause(1);
+
+            driveNewIMU(12.5, 2.7, -.3, false, 200);
         }
 
-        pause(.025); //pause for .025 seconds
+        pause(.1);
 
-        touch(false, false, false); //Run touch method with parameters false, false, and false, meaning we are running from red not corner position
-
-        pause(.025); //Pause for .025 seconds
 
         if (vuMarkReturn.equalsIgnoreCase("left")) //If vuforia reads left
         {
-
-        } else if (vuMarkReturn.equalsIgnoreCase("right")) //If vuforia reads right
-        {
-            strafeNewIMU(16.4, 5, -.65, 13); //Strafe to the left for 16.4 inches at -.65 power keeping a 13 degree heading with a 5 second limit
-        } else //else
-        {
-            strafeNewIMU(10, 5, -.65, 13); //Strafe to the left for 13.2 inches at -.63 power keeping a 13 degree heading with a 5 second limit
+            driveNewIMU(3, 5, .3, true, -230);
         }
-
-        if (this.getRuntime() - threeGlyphTimeOne < 17.5) { //If enough time is left
-
-            spinMove(152, false, 4, false); //Spin move to 152 degrees to position for intaking blocks, not starting at .3 power and not putting the touchServo down with a 4 second timeout
-
-            if (this.getRuntime() - threeGlyphTimeOne < 21.5) { //If enough time is left
-                getBlocks(60); //getBlocks with 60 inch distance
-            }
-
-            pause(.05); //Pause for .05 seconds
-
-            if (this.getRuntime() - threeGlyphTimeOne < 30.8) { //If enough time is left
-                if (sensorB.getDistance(DistanceUnit.CM) < 10 || sensorC.getDistance(DistanceUnit.CM) < 10) { //If we have at least 1 glyph
-                    scoreBlock(160); //score Block at 160 degrees
-                } else { //Else if we do not have glyph
-                    if (this.getRuntime() - threeGlyphTimeOne < 50.8) { //If enough time is left
-                        driveNewIMU(4.8, 1.5, .3, true, 160);
-                    }
-                }
-            }
-
+        else if (vuMarkReturn.equalsIgnoreCase("right")) //If vuforia reads right
+        {
+            driveNewIMU(3, 5, .3, true, -150);
+        }
+        else //else
+        {
+            driveNewIMU(3, 5, .3, true, 200);
         }
 
 
+
+
+//        telemetry.addData("time elapsed", this.getRuntime()-threeGlyphTimeOne); //Telemetry for time elapsed
+//        telemetry.update(); //Update telemetry
+//
+//        if (this.getRuntime() - threeGlyphTimeOne < 17.5) { //If enough time is left
+//
+//            spinMove(152, false, 4, false); //Spin move to 152 degrees to position for intaking blocks, not starting at .3 power and not putting the touchServo down with a 4 second timeout
+//
+//            if (this.getRuntime() - threeGlyphTimeOne < 20.5) { //If enough time is left
+//                getBlocks(60); //getBlocks with 60 inch distance
+//            }
+//
+//            pause(.05); //Pause for .05 seconds
+//
+//            if (this.getRuntime() - threeGlyphTimeOne < 26) { //If enough time is left
+//                if (sensorB.getDistance(DistanceUnit.CM) < 10 || sensorC.getDistance(DistanceUnit.CM) < 10) { //If we have at least 1 glyph
+//                    scoreBlock(160); //score Block at 160 degrees
+//                } else { //Else if we do not have glyph
+//                    if (this.getRuntime() - threeGlyphTimeOne < 27.5) { //If enough time is left
+//                        driveNewIMU(4.8, 1.5, .3, true, 160);
+//                    }
+//                }
+//            }
+//
+//        }
+//
+//
     }
 
 }
